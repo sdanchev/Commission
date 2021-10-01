42ade29c6f5c8a664830b9f8c39a38618e64f411
Sava Danchev <sdanchev4090@stu.d214.org>
2021-10-01 15:03:10 -0500

    Storyboard and Controller

--- Commission/ViewController.swift
+++ Commission/ViewController.swift
@@ -9,11 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var BasePayLabel: UILabel!
+    @IBOutlet weak var CommissionPayTextField: UITextField!
+    @IBOutlet weak var TotalPayLabel: UILabel!
+    
+    let basePay = 500.0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        BasePayLabel.text = "$\(basePay)"
+        TotalPayLabel.text = ""
+        
     }
 
-
+    @IBAction func whenButtonPressed(_ sender: Any) {
+        let data = CommissionPayTextField.text!
+        let commissionPay = Double(data)!
+        let totalPay = commissionPay + basePay
+        TotalPayLabel.text = "$\(totalPay)"
+        
+    }
+    
 }
 
